4c2ee8ea4bae5b43e60d129e4c4871f0d0284ab4
James McCartney <mccartneyj2009@gmail.com>
2021-12-13 22:18:17 -0500

    Added gamePlay function and startGame function. gamePlay incomplete.

--- app.js
+++ app.js
@@ -1,16 +1,85 @@
 // Query selectors
-board = document.querySelector(".board")
-player1Marker = document.querySelector(".player-markers-selection").value
-player2Marker = document.querySelector(".pl")
-console.log(player1Marker);
+const gameState = {
+    players: {
+        player1: "Player 1",
+        player2: "Player 2",
+    },
+    markers: {
+        player1: "X",
+        player2: "O",
+    },
+    gameBoard: [[null, null, null, null, null, null, null, null, null]],
+    playerUp: 1,
+};
 
-function gameSetup(){
-    message = document.querySelector("#in-game-message");
-    message.innerHTML = 'Click "Start Game" below';
+let board = document.querySelector(".board");
+let message = document.querySelector("#in-game-message");
+let player1Marker = document.querySelector("#player-1-marker");
+let player2Marker = document.querySelector("#player-2-marker");
+let player1Name = document.querySelector("#player-1-name");
+let player2Name = document.querySelector("#player-2-name");
+let startGameBtn = document.querySelector("#start-game-btn");
+const cellValues = Array.from(document.querySelectorAll(".cell"));
+
+function gamePlay(e) {
+    if (e.target.innerHTML !== "") {
+        message.innerHTML = "Pick an empty cell";
+    } else {
+        if (gameState.playerUp === 1) {
+            e.target.innerHTML = gameState.markers.player1;
+            gameState.playerUp = 2;
+            message.innerHTML = `${gameState.players.player2}'s Turn`;
+        } else if (gameState.playerUp === 2) {
+            e.target.innerHTML = gameState.markers.player2;
+            gameState.playerUp = 1;
+            message.innerHTML = `${gameState.players.player1}'s Turn`;
+        }
+    }
+
+    for (let cell in cellValues) {
+        gameState.gameBoard[cell] = cellValues[cell].innerHTML;
+    }
+
+    if (
+        gameState.gameBoard[0] === gameState.gameBoard[3] &&
+        gameState.gameBoard[0] === gameState.gameBoard[6]
+    ) {
+        console.log("Winner");
+    }
+    if (
+        gameState.gameBoard[0] === gameState.gameBoard[3] &&
+        gameState.gameBoard[0] === gameState.gameBoard[6]
+    ) {
+        console.log("Winner");
+    }
 }
 
-board.addEventListener("click", function(e){
-    console.log(e.target);
-})
+function startGame() {
+    if (player1Name.value !== "") {
+        gameState.players.player1 = player1Name.value;
+    }
+    if (player2Name.value !== "") {
+        gameState.players.player2 = player2Name.value;
+    }
 
-gameSetup()
+    if (player1Marker.value !== "none") {
+        gameState.markers.player1 = player1Marker.value;
+    }
+    if (player2Marker.value !== "none") {
+        gameState.markers.player2 = player2Marker.value;
+    }
+
+    startGameBtn.removeEventListener("click", startGame);
+    startGameBtn.innerHTML = "Reset Game";
+
+    player1Name.disabled = true;
+    player2Name.disabled = true;
+    player1Marker.disabled = true;
+    player2Marker.disabled = true;
+
+    message.innerHTML = `${gameState.players.player1}'s Turn`;
+
+    board.addEventListener("click", gamePlay);
+}
+
+startGameBtn.addEventListener("click", startGame);
