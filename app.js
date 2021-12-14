1ab6a90a0744df6771a9363ce1dfb8b3be43c39e
James McCartney <48179143+mccartneyj2009@users.noreply.github.com>
2021-12-14 15:18:53 -0500

    changes made to gamePlay function

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // Query selectors
 const gameState = {
-    players: {
-        player1: "Player 1",
-        player2: "Player 2",
-    },
-    markers: {
-        player1: "X",
-        player2: "O",
-    },
-    gameBoard: [[null, null, null, null, null, null, null, null, null]],
-    playerUp: 1,
+  players: {
+    player1: "Player 1",
+    player2: "Player 2",
+  },
+  markers: {
+    player1: "X",
+    player2: "O",
+  },
+  gameBoard: [null, null, null, null, null, null, null, null, null],
+  playerUp: 1,
 };
 
 let board = document.querySelector(".board");
@@ -22,64 +22,105 @@ let startGameBtn = document.querySelector("#start-game-btn");
 const cellValues = Array.from(document.querySelectorAll(".cell"));
 
 function gamePlay(e) {
-    if (e.target.innerHTML !== "") {
-        message.innerHTML = "Pick an empty cell";
-    } else {
-        if (gameState.playerUp === 1) {
-            e.target.innerHTML = gameState.markers.player1;
-            gameState.playerUp = 2;
-            message.innerHTML = `${gameState.players.player2}'s Turn`;
-        } else if (gameState.playerUp === 2) {
-            e.target.innerHTML = gameState.markers.player2;
-            gameState.playerUp = 1;
-            message.innerHTML = `${gameState.players.player1}'s Turn`;
-        }
-    }
+  if (e.target.innerHTML !== "") {
+    message.innerHTML = "Pick an empty cell";
+  } else {
+    if (gameState.playerUp === 1) {
+      e.target.innerHTML = gameState.markers.player1;
+      gameState.playerUp = 2;
 
-    for (let cell in cellValues) {
-        gameState.gameBoard[cell] = cellValues[cell].innerHTML;
+      message.innerHTML = `${gameState.players.player2}'s Turn`;
+    } else if (gameState.playerUp === 2) {
+      e.target.innerHTML = gameState.markers.player2;
+      gameState.playerUp = 1;
+      message.innerHTML = `${gameState.players.player1}'s Turn`;
     }
+  }
 
-    if (
-        gameState.gameBoard[0] === gameState.gameBoard[3] &&
-        gameState.gameBoard[0] === gameState.gameBoard[6]
-    ) {
-        console.log("Winner");
+  for (let i = 0; i < 3; i++) {
+    if (gameState.gameBoard[i] === "X") {
     }
-    if (
-        gameState.gameBoard[0] === gameState.gameBoard[3] &&
-        gameState.gameBoard[0] === gameState.gameBoard[6]
-    ) {
-        console.log("Winner");
-    }
+  }
+  console.log(cellValues, gameState.gameBoard);
+  // Column win combinations
+  //   if (
+  //     gameState.gameBoard[0] === gameState.gameBoard[3] &&
+  //     gameState.gameBoard[0] === gameState.gameBoard[6]
+  //   ) {
+  //     console.log("Winner");
+  //   }
+  //   if (
+  //     gameState.gameBoard[1] === gameState.gameBoard[4] &&
+  //     gameState.gameBoard[1] === gameState.gameBoard[7]
+  //   ) {
+  //     console.log("Winner");
+  //   }
+  //   if (
+  //     gameState.gameBoard[2] === gameState.gameBoard[5] &&
+  //     gameState.gameBoard[2] === gameState.gameBoard[8]
+  //   ) {
+  //     console.log("Winner");
+  //   }
+  //   //Row win combinations
+  //   if (
+  //     gameState.gameBoard[0] === gameState.gameBoard[1] &&
+  //     gameState.gameBoard[0] === gameState.gameBoard[2]
+  //   ) {
+  //     console.log("Winner");
+  //   }
+  //   if (
+  //     gameState.gameBoard[3] === gameState.gameBoard[4] &&
+  //     gameState.gameBoard[3] === gameState.gameBoard[5]
+  //   ) {
+  //     console.log("Winner");
+  //   }
+  //   if (
+  //     gameState.gameBoard[6] === gameState.gameBoard[7] &&
+  //     gameState.gameBoard[6] === gameState.gameBoard[8]
+  //   ) {
+  //     console.log("Winner");
+  //   }
+  //   //Diagonal win combinations
+  //   if (
+  //     gameState.gameBoard[0] === gameState.gameBoard[4] &&
+  //     gameState.gameBoard[0] === gameState.gameBoard[8]
+  //   ) {
+  //     console.log("Winner");
+  //   }
+  //   if (
+  //     gameState.gameBoard[2] === gameState.gameBoard[4] &&
+  //     gameState.gameBoard[2] === gameState.gameBoard[6]
+  //   ) {
+  //     console.log("Winner");
+  //   }
 }
 
 function startGame() {
-    if (player1Name.value !== "") {
-        gameState.players.player1 = player1Name.value;
-    }
-    if (player2Name.value !== "") {
-        gameState.players.player2 = player2Name.value;
-    }
+  if (player1Name.value !== "") {
+    gameState.players.player1 = player1Name.value;
+  }
+  if (player2Name.value !== "") {
+    gameState.players.player2 = player2Name.value;
+  }
 
-    if (player1Marker.value !== "none") {
-        gameState.markers.player1 = player1Marker.value;
-    }
-    if (player2Marker.value !== "none") {
-        gameState.markers.player2 = player2Marker.value;
-    }
+  if (player1Marker.value !== "none") {
+    gameState.markers.player1 = player1Marker.value;
+  }
+  if (player2Marker.value !== "none") {
+    gameState.markers.player2 = player2Marker.value;
+  }
 
-    startGameBtn.removeEventListener("click", startGame);
-    startGameBtn.innerHTML = "Reset Game";
+  startGameBtn.removeEventListener("click", startGame);
+  startGameBtn.innerHTML = "Reset Game";
 
-    player1Name.disabled = true;
-    player2Name.disabled = true;
-    player1Marker.disabled = true;
-    player2Marker.disabled = true;
+  player1Name.disabled = true;
+  player2Name.disabled = true;
+  player1Marker.disabled = true;
+  player2Marker.disabled = true;
 
-    message.innerHTML = `${gameState.players.player1}'s Turn`;
+  message.innerHTML = `${gameState.players.player1}'s Turn`;
 
-    board.addEventListener("click", gamePlay);
+  board.addEventListener("click", gamePlay);
 }
 
 startGameBtn.addEventListener("click", startGame);
